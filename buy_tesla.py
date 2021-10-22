f9be17c00a0e5161325cb1aa64f604b3b3d621d4
EC2 Default User <ec2-user@ip-172-31-22-75.ec2.internal>
2021-10-22 16:55:59 +0000

    updataed script

--- buy_tesla.py
+++ buy_tesla.py
@@ -1,8 +1,7 @@
-print('Tesla offers four car models: Model S, Model X, Model 3, and Model Y')
-print('I would always go for Model Y because of uniqueness')
 import pandas as pd
-url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
+import sys
+
+file_to_open = open(sys.argv[1], 'r')
+#url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
 data = pd.read_csv(url, sep =',', index_col=0)
-data.head()
-data.describe()
 print(data.describe())
